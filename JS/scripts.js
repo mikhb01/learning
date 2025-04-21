d941a27d1bf0977a1db0ac268dce1d9c2a85bf7b
mikhb01 <143737949+mikhb01@users.noreply.github.com>
2025-04-21 15:57:33 +0900

    Fethching JSON file adding function to fetch ingredients of recipes

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,4 @@
+// Box resize function after clicking "show more" or "show less"
 window.onresize =function(){
     if(window.innerWidth <= 600){
         document.getElementById("box1").style.width = "100%";
@@ -10,12 +11,65 @@ window.onresize =function(){
     }
 }
 
-function showMore(){
+// For testing if fetching works
+//
+// fetch("JSON/recipe.JSON")
+//     .then(response => {
+//         if(!response.ok) {
+//             throw new Error("Network response was not ok");
+//         }
+//         return response.json(); //JSON parsing the respone
+//     })
+//     .then(data =>{
+//         console.log(data);
+//         console.log(data.name);
+//     })
+//     .catch(error => {
+//         console.error("There was a problem with the fetch operation", error);
+//     });
+
+// Show more/ show less function of buttons
+//Included in it: loading of data and recipe from a JSON file 
+
+//No new loading of the data after the button was pressed once
+let schonGeladen1 = false;
+let schonGeladen2 = false;
+let schonGeladen3 = false;
+
+    
+function showMore(){    
+
+    async function loadRecipe() {
+        if(schonGeladen1) return;
+
+        schonGeladen1 = true;
+
+        const response = await fetch("/JSON/recipe.JSON", {headers:{'Accept': 'application/json'}});
+        const liste = document.getElementById("ing1");
+
+
+
+        let text = await response.json();
+        let jsonString = JSON.stringify(text);
+    
+        let obj = JSON.parse(jsonString);
+        console.log(obj);
+
+        for(i = 0; i < obj.recipes[0].ingredients.length; i++){
+            console.log(obj.recipes[0].ingredients[i]);
+            let li = document.createElement("li");
+            li.textContent = obj.recipes[0].ingredients[i];
+            console.log(li);
+            liste.appendChild(li);
+            }
+    }
     document.getElementById("recipes1").style.display = "block";
     document.getElementById("box1").style.width = "100%";
     document.getElementById("b1").style.display = "none";
     document.getElementById("box2").style.display = "none";
     document.getElementById("box3").style.display = "none";
+    
+    loadRecipe();
 }
 
 function showLess(){
@@ -30,11 +84,39 @@ function showLess(){
 }
 
 function showMore2(){
+
+    async function loadRecipe() {
+        if(schonGeladen2) return;
+
+        schonGeladen2 = true;
+
+        const response = await fetch("/JSON/recipe.JSON", {headers:{'Accept': 'application/json'}});
+        const liste = document.getElementById("ing2");
+
+
+
+        let text = await response.json();
+        let jsonString = JSON.stringify(text);
+    
+        let obj = JSON.parse(jsonString);
+        console.log(obj);
+
+        for(i = 0; i < obj.recipes[1].ingredients.length; i++){
+            console.log(obj.recipes[1].ingredients[i]);
+            let li = document.createElement("li");
+            li.textContent = obj.recipes[1].ingredients[i];
+            console.log(li);
+            liste.appendChild(li);
+            }
+    }
+
     document.getElementById("recipes2").style.display = "block";
     document.getElementById("box2").style.width = "100%";
     document.getElementById("b2").style.display = "none";
     document.getElementById("box1").style.display = "none";
     document.getElementById("box3").style.display = "none";
+
+    loadRecipe();
 }
 
 function showLess2(){
@@ -49,20 +131,48 @@ function showLess2(){
 }
 
 function showMore3(){
+
+    async function loadRecipe() {
+        if(schonGeladen3) return;
+
+        schonGeladen3 = true;
+
+        const response = await fetch("/JSON/recipe.JSON", {headers:{'Accept': 'application/json'}});
+        const liste = document.getElementById("ing3");
+
+
+
+        let text = await response.json();
+        let jsonString = JSON.stringify(text);
+    
+        let obj = JSON.parse(jsonString);
+        console.log(obj);
+
+        for(i = 0; i < obj.recipes[2].ingredients.length; i++){
+            console.log(obj.recipes[2].ingredients[i]);
+            let li = document.createElement("li");
+            li.textContent = obj.recipes[2].ingredients[i];
+            console.log(li);
+            liste.appendChild(li);
+            }
+    }
+
     document.getElementById("recipes3").style.display = "block";
     document.getElementById("box3").style.width = "100%";
     document.getElementById("b3").style.display = "none";
     document.getElementById("box1").style.display = "none";
     document.getElementById("box2").style.display = "none";
+
+    loadRecipe();
+
 }
 
 function showLess3(){
-        document.getElementById("recipes3").style.display = "none";
-        document.getElementById("b3").style.display = "inline-block";
-        document.getElementById("box2").style.display = "block";
-        document.getElementById("box1").style.display = "block";
-
-        if(window.innerWidth > 600){
-            document.getElementById("box3").style.width = "33.33%";
-        }
+    document.getElementById("recipes3").style.display = "none";
+    document.getElementById("b3").style.display = "inline-block";
+    document.getElementById("box2").style.display = "block";
+    document.getElementById("box1").style.display = "block";
+    if(window.innerWidth > 600){
+        document.getElementById("box3").style.width = "33.33%";
+    }
 }
